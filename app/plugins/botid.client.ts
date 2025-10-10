4582a396327dcab66d3b307804494bff1c234631
AJ Arshem <ajarshem@gmail.com>
2025-10-10 08:55:18 -0500

    enforce: 'pre'

--- app/plugins/botid.client.ts
+++ app/plugins/botid.client.ts
@@ -1,12 +1,12 @@
 import { initBotId } from 'botid/client/core';
- 
-export default defineNuxtPlugin({
 
-  setup() {
-    console.log('Initializing BotId plugin...');
-    initBotId({
-      protect: [{ path: '/api/contact/test', method: 'POST' }],
-    });
-    console.log('BotId plugin initialized.');
-  },
+export default defineNuxtPlugin({
+    enforce: 'pre',
+    setup() {
+        console.log('Initializing BotId plugin...');
+        initBotId({
+            protect: [{ path: '/api/contact/test', method: 'POST' }],
+        });
+        console.log('BotId plugin initialized.');
+    },
 });
